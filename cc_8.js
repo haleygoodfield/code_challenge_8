dec9fa58cb154e4f059ed09309d0fba3c88dafb6
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-13 19:44:03 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -3,6 +3,20 @@
 function calculateSalary(baseSalary, bonus, taxRate) {
     let netSalary = (baseSalary + bonus) - (baseSalary * taxRate); // Formula
     return `Net Salary: $${netSalary.toFixed(2)}`;
-}
+};
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00
+
+
+
+// Task 2: Function Expression 
+// Declare a function expression calculateDiscount(price, discountRate) to determine the final price
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate); // Formula
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+};
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50
+
+
+
